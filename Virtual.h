714d9c438775ce1034204bea7dc92dd0c3f4d36f
Maybe <dzwduan@gmail.com>
2019-02-23 10:29:47 +0800

    Create Virtual.h

---
+++ Virtual.h
@@ -0,0 +1,19 @@
+﻿
+#ifndef Virtual_H
+#define Virtual_H
+#include<iostream>
+
+class Shape {
+public:
+	//纯虚函数，希望子类一定要重新定义
+	virtual void draw() const = 0; 
+	//虚函数，希望子类重新定义，且基类已有默认定义
+	virtual void error(const std::string &msg);
+	//非虚函数
+	int objectID() const;
+};
+
+class Rectangle :public Shape {};
+class Ellipse: public Shape{};
+
+#endif 
